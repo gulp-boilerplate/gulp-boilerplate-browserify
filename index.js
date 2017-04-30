449585bfa52c7ab1943b36c904843fa62d460f0e
Marcos Hernández <marcoslhc@gmail.com>
2017-04-30 09:37:52 -0400

    Allow other transforms, update package

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     exorcist = require('exorcist');
 
-module.exports = function (config) {
+module.exports = function ({ options, transforms, dest }) {
     return function () {
-         return browserify(config.options)
-                .transform('coffeeify')
-                .transform({sourcemap:true, global: true}, 'uglifyify')
+         const bundle = browserify(options);
+
+        if (transforms && transforms.length > 1) {
+            bundle = transforms.reduce((bunle, transform) => {
+                let transformArguments = transform;
+                if (!isArray(transformArguments)) {
+                    transformArguments = [transformArguments];
+                }
+                return bundle.transform.apply(bundle, transformArguments);
+            }, bundle);
+        }
+         return bundle
                 .bundle()
-                .pipe(exorcist(config.dest + '/main.js.map'))
+                .pipe(exorcist(dest + '/main.js.map'))
                 .pipe(source('main.js'))
-                .pipe(gulp.dest(config.dest));
+                .pipe(gulp.dest(dest));
     };
 };
